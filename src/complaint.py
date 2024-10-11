af5256070b0ff61ffdbc3ef7dc1499f5a5e5c16c
Сергей <osedchii.serega48@gmail.com>
2024-10-11 16:43:08 +0300

    run v1

--- src/complaint.py
+++ src/complaint.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+@dataclass
 class Complaint:
 
     message: str
---
+++ src/run.py
@@ -0,0 +1,31 @@
+from technical_support_handler import TechnicalSupportHandler
+from manager_support_handler import ManagerSupportHandler
+from basic_support_handler import BasicSupportHandler
+from interface import ComplaintHandler
+from complaint import Complaint
+
+from typing import List
+
+complaints = [
+    Complaint("У меня вопрос о продукте.", "basic"),
+    Complaint("Программа выдает ошибку.", "technical"),
+    Complaint("Серьезная проблема с заказом.", "serious"),
+    Complaint("Не могу войти в аккаунт.", "unknown"),
+]
+
+
+def client_code(complaints: List):
+    basic_handler = BasicSupportHandler()
+    tech_handler = TechnicalSupportHandler()
+    manager_handler = ManagerSupportHandler()
+
+    basic_handler.successor = tech_handler
+    tech_handler.successor = manager_handler
+
+    for complaint in complaints:
+        print(f"Обработка жалобы: {complaint.message}")
+        basic_handler.handle_complaint(complaint)
+
+
+if __name__ == "__main__":
+    client_code(complaints)
